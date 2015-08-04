bcfd00059c69c8d520b629a979e99502bd32e944
Oleg-Kononenko <kononenko@active-bridge.com>
2015-08-04 17:27:51 +0300

    Update Elasticsearch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+
+  resources :posts
+  resource :search, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
